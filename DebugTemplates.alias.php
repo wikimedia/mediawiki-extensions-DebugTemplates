2c0aca49c1d09cf8cc891f56ab97ca0cc8771e9b
anterdc99 <xuminruixuan@live.cn>
2025-07-15 22:55:21 +0800

    DebugTemplates: Translate special page aliases into Chinese
    Change-Id: I1177edfd86b1f36c97950fcbbdc71e4fb46e9e1d

--- DebugTemplates.alias.php
+++ DebugTemplates.alias.php
@@ -12,3 +12,18 @@ $specialPageAliases = [];
 $specialPageAliases['en'] = [
 	'DebugTemplates' => [ 'DebugTemplates', 'Debug Templates' ],
 ];
+
+/** Chinese (中文) */
+$specialPageAliases['zh'] = [
+	'DebugTemplates' => [ 'DebugTemplates' ],
+];
+
+/** Simplified Chinese (中文（简体）) */
+$specialPageAliases['zh-hans'] = [
+	'DebugTemplates' => [ '模板调试', '调试模板' ],
+];
+
+/** Traditional Chinese (中文（繁體）) */
+$specialPageAliases['zh-hant'] = [
+	'DebugTemplates' => [ '模板除錯' ],
+];
